ec0e5645e28074e45d90e771a1334d698a3ae6cc
Mikael-RajaKhan <168760324+Mikael-RajaKhan@users.noreply.github.com>
2024-06-07 21:48:41 +0100

    Add files via upload

--- search.php
+++ search.php
@@ -1,5 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to BrightDragon!</title>
 
-echo $_GET ["term"];
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
-?>
+
+</head>
+<body>
+
+<div class="wrapper">
+    <h1><?php echo $_GET['term']; ?></h1>
+
+    <div class="header">
+
+        <div class="headerContent">
+
+            
+
+    </div>
+
+    </div>
+
+</body>
+</html>
